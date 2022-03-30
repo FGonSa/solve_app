b729dfcd1a8f1be01ab3a282a614a467337415b4
Iago de Oliveira <bcniago@gmail.com>
2022-03-30 11:11:53 +0200

    cambios colores + localhost
    hay que comentar el que no se uso y descomentar el que se quiera usar

--- vue/src/components/ContainerAjustes.vue
+++ vue/src/components/ContainerAjustes.vue
@@ -3,7 +3,7 @@
         <div class="contenedor-header mb-2" :style="color">
             <router-link :to="url" class="contenedor-btn">
 				<img :src="imagen">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="flecha-blanca bi bi-arrow-right-short " viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#393939" class="flecha-blanca bi bi-arrow-right-short " viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
                 </svg>
 			</router-link>
@@ -12,7 +12,7 @@
             <h5>{{ titulo }}</h5>
         </div>
     </div>
-    
+
 </template>
 
 <script>
@@ -33,7 +33,7 @@ export default {
     // font-family: 'Poppins', sans-serif;
     justify-content: center;
     align-items: center;
-    color: #645961
+    color: #645961;
 }
 .container-main-ajustes{
     display: flex;
@@ -45,7 +45,7 @@ export default {
 }
 .contenedor-header{
     background: rgb(185,47,115);
-    background: linear-gradient(0deg, rgba(185,47,115,1) 0%, var(--color-rosa) 45%, rgba(247,111,178,1) 100%); 
+    background: linear-gradient(0deg, rgba(185,47,115,1) 0%, var(--color-rosa) 45%, rgba(247,111,178,1) 100%);
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     border-radius: 30px;
     display: flex;
@@ -63,14 +63,14 @@ export default {
         gap: 0.5rem;
 
         img{
-        
+
         width: 100px;
         height: 100px;
     }
     }
 
-    
-    
+
+
 }
 .contenedor-header:hover{
     opacity:75%;
